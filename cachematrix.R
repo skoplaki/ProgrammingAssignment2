f1746b5c9439f1ad01f8113bcb0b51d0446929cd
skoplaki <skoplaki@users.noreply.github.com>
2014-11-22 21:38:19 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set the enviroment and calculate the inverser matrix
 
-## Write a short comment describing this function
+## setting the enviroment for lexical scoping
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function(){
+    return (x)
+  }
+  
+  setinverse <- function(yy){
+    m <<- yy
+  }
+  
+  getinverse <- function(){
+    return (m)
+  }
+  
+  list(set= set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
-
-## Write a short comment describing this function
+## If matrix_inversed is not NULL it will be computed, 
+## if not the old matrix_inversed will be returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  matrix_inversed <- x$getinverse()
+  
+  if(!is.null(matrix_inversed)) {
+    message("Getting cached data")
+    return(matrix_inversed)
+  }
+  
+  data <- x$get()
+  matrix_inversed <- solve(data, ...)
+  x$setinverse(matrix_inversed)
+  return(matrix_inversed)
 }
